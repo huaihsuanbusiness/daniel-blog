d42860c59298e6e24e25b8f42868b221e4ab1176
Daniel <ilj9549@gmail.com>
2026-03-31 16:22:12 +0800

    fix: restore static astro i18n deployment mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   image: {
     service: passthroughImageService(),
   },
-
   output: 'static',
   site: 'https://danielcanfly.com',
-
   build: {
     assets: '_assets',
   },
-
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
@@ -21,12 +16,9 @@ export default defineConfig({
       prefixDefaultLocale: true,
     },
   },
-
   vite: {
     build: {
       cssMinify: true,
     },
   },
-
-  adapter: cloudflare(),
 });
